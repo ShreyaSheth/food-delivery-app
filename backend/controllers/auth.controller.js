629e0231467a7a7639d91f6bfd5c00ed0ceb0e66
ShreyaSheth <shreya.sheth@globalvoxinc.com>
2025-09-18 00:08:42 +0530

    Connected databse and added user schema , created signup controller

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,49 @@
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import generateToken from "../utils/token.js";
+
+const signUp = async (req, res) => {
+  try {
+    const { firstName, lastName, email, password, mobile, role } = req.body;
+    const user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ message: "User already exists." });
+    }
+    const passwordRegex =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+    if (!passwordRegex.test(password)) {
+      return res.status(400).json({
+        message:
+          "Password must be at least 8 characters long, include one uppercase, one lowercase, one number, and one special character.",
+      });
+    }
+    if (mobile.length !== 10) {
+      return res
+        .status(400)
+        .json({ message: "Mobile number must be 10 digits long." });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    user = await User.create({
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword,
+      mobile,
+      role,
+    });
+    const token = await generateToken(user._id);
+
+    res.cookie("token", token, {
+      secure: false,
+      sameSite: "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+      httpOnly: true,
+    });
+
+    return res.status(201).json({ message: "User created successfully." });
+  } catch (error) {
+    return res.status(500).json({ message: `SignUp Error: ${error}` });
+  }
+};
