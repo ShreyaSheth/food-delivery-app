b9b03c0e59efebb40d8698851fc60d5e9cdc13c4
ShreyaSheth <shreya.sheth@globalvoxinc.com>
2025-09-18 18:27:01 +0530

    Added signIn and signOut cotrollers

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -2,10 +2,10 @@ import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import generateToken from "../utils/token.js";
 
-const signUp = async (req, res) => {
+export const signUp = async (req, res) => {
   try {
     const { firstName, lastName, email, password, mobile, role } = req.body;
-    const user = await User.findOne({ email });
+    let user = await User.findOne({ email });
     if (user) {
       return res.status(400).json({ message: "User already exists." });
     }
@@ -45,5 +45,40 @@ const signUp = async (req, res) => {
     return res.status(201).json({ message: "User created successfully." });
   } catch (error) {
     return res.status(500).json({ message: `SignUp Error: ${error}` });
+  }
+};
+
+export const signIn = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "User does not exist." });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials." });
+    }
+    const token = await generateToken(user._id);
+
+    res.cookie("token", token, {
+      secure: false,
+      sameSite: "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+      httpOnly: true,
+    });
+
+    return res.status(200).json(user);
+  } catch (error) {
+    return res.status(500).json({ message: `SignIn Error: ${error}` });
+  }
+};
+
+export const signOut = async (req, res) => {
+  try {
+    res.clearCookie("token");
+    return res.status(200).json({ message: "User signed out successfully." });
+  } catch (error) {
+    return res.status(500).json({ message: `SignOut Error: ${error}` });
   }
 };
